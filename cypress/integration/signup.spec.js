7fe24e9bc6f256df33adbafcaee594de271e35d7
Suyanne França <suyfranca@gmail.com>
2022-05-07 13:14:56 -0300

    Change to english language

---
+++ cypress/integration/signup.spec.js
@@ -0,0 +1,28 @@
+import signup from '../pages/SignupPage';
+
+describe('Signup', () => { 
+
+    beforeEach( function() {
+        cy.fixture('deliver').then( (data) => {
+            this.deliver = data;
+        });
+    });
+
+    it('User should be deliver', function() { 
+        signup.go();
+        signup.fillForm(this.deliver.signup);
+        signup.submit();
+        
+        const expectedRegisterMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
+        signup.modalContentShouldBe(expectedRegisterMessage);
+    })
+
+    it('Invalid document', function() { 
+        signup.go();
+        signup.fillForm(this.deliver.cpf_inv);
+        signup.submit();
+        
+        const alertErrorMessage = 'Oops! CPF inválido';
+        signup.alertContentShouldBe(alertErrorMessage);
+    })
+})
