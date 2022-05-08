889fa2fee7bebb0a8621acab6353c116f4f6fcf9
Suyanne França <suyfranca@gmail.com>
2022-05-08 08:22:38 -0300

    Modify alertContentShouldBe

--- cypress/pages/SignupPage.js
+++ cypress/pages/SignupPage.js
@@ -38,7 +38,8 @@ class SignupPage {
     }
 
     alertContentShouldBe(expectedMessage) {
-        cy.get('.alert-error').should('have.text', expectedMessage);
+        //cy.get('.alert-error').should('have.text', expectedMessage);
+        cy.contains('.alert-error', expectedMessage).should('be.visible');
     }
 }
 
