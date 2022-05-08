35a0c7f16f13885015128b6c15b7634026af2e98
Suyanne França <suyfranca@gmail.com>
2022-05-08 09:21:03 -0300

    Refactor the test cases and add context test required fields

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,37 +1,74 @@
-import signup from '../pages/SignupPage';
+import signup from "../pages/SignupPage";
+import signupFactory from "../factories/SignupFactory";
 
-describe('Signup', () => { 
+describe("Signup", () => {
+  // beforeEach( function() {
+  //     cy.fixture('deliver').then( (data) => {
+  //         this.deliver = data;
+  //     });
+  // });
 
-    beforeEach( function() {
-        cy.fixture('deliver').then( (data) => {
-            this.deliver = data;
+  it("User should be deliver", function () {
+    let deliver = signupFactory.deliver();
+
+    signup.go();
+    signup.fillForm(deliver);
+    signup.submit();
+
+    const expectedRegisterMessage =
+      "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.";
+    signup.modalContentShouldBe(expectedRegisterMessage);
+  });
+
+  it("Invalid document", function () {
+    let deliver = signupFactory.deliver();
+
+    deliver.cpf = "000000141aa";
+
+    signup.go();
+    signup.fillForm(deliver);
+    signup.submit();
+
+    const alertErrorMessage = "Oops! CPF inválido";
+    signup.alertContentShouldBe(alertErrorMessage);
+  });
+
+  it("Invalid email", function () {
+    let deliver = signupFactory.deliver();
+
+    deliver.email = "user.com.br";
+
+    signup.go();
+    signup.fillForm(deliver);
+    signup.submit();
+
+    const alertErrorMessage = "Oops! Email com formato inválido.";
+    signup.alertContentShouldBe(alertErrorMessage);
+  });
+
+  context("Required fields", function () {
+
+    const messages = [
+      { field: "name", output: "É necessário informar o nome" },
+      { field: "cpf", output: "É necessário informar o CPF" },
+      { field: "email", output: "É necessário informar o email" },
+      { field: "postalcode", output: "É necessário informar o CEP" },
+      { field: "number", output: "É necessário informar o número do endereço" },
+      { field: "delivery_method", output: "Selecione o método de entrega" },
+      { field: "cnh", output: "Adicione uma foto da sua CNH" }
+    ];
+
+    before(function(){
+        signup.go();
+        signup.submit();
+    });
+
+    messages.forEach( function(msg) {
+        it(`${msg.field} is required`, function(){
+            signup.alertContentShouldBe(msg.output);
         });
     });
 
-    it('User should be deliver', function() { 
-        signup.go();
-        signup.fillForm(this.deliver.signup);
-        signup.submit();
-        
-        const expectedRegisterMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.';
-        signup.modalContentShouldBe(expectedRegisterMessage);
-    });
+  });
 
-    it('Invalid document', function() { 
-        signup.go();
-        signup.fillForm(this.deliver.cpf_inv);
-        signup.submit();
-        
-        const alertErrorMessage = 'Oops! CPF inválido';
-        signup.alertContentShouldBe(alertErrorMessage);
-    });
-
-    it('Invalid email', function() { 
-        signup.go();
-        signup.fillForm(this.deliver.email_inv);
-        signup.submit();
-        
-        const alertErrorMessage = 'Oops! Email com formato inválido.';
-        signup.alertContentShouldBe(alertErrorMessage);
-    });
-})
+});
